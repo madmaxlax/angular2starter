b07505f480c99df167c3046ee32474c1192b61f1
Struever <Maxwell.C.Struever@shell.com>
2016-08-16 10:37:20 +0200

    changes

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1><h3>Loaded successfully!</h3>'
+})
+export class AppComponent { }
